bef8697e27203f147b4b781d6506ba36e6769700
Joshua M. Maldonado <jm998521@ohio.edu>
2025-07-29 14:47:50 -0400

    Add files via upload

--- chat_specs.h
+++ chat_specs.h
@@ -1,6 +1,7 @@
 /* Author: Joshua Maldonado */
 
-const int PORT = 8080;      // max number of connections to the server at a time
-const int MAX_CLIENTS = 4;  // port to connect to
-const int MAX = 256;        // max message size
-const int NAME_SIZE = 24;   // max size for usernames
+const int PORT = 8080;       // max number of connections to the server at a time
+const int MAX_CLIENTS = 4;   // port to connect to
+const int MAX = 256;         // max message size
+const int NAME_SIZE = 24;    // max size for usernames
+const int COMMAND_SIZE = 8;  // max size for commands (ex. @whisper or @exit)
